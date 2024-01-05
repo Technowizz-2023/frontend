54f018b7e483b4fa878217dd05ee0e11ce3d0ba7
Jaromír Trnka <JrTrnka55@gmail.com>
2024-01-05 16:55:03 +0100

    added HTMX CDN

--- src/components/BaseHtml.tsx
+++ src/components/BaseHtml.tsx
@@ -9,6 +9,7 @@ export const BaseHtml = () => `
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>BUNJS</title>
         <link rel="stylesheet" href="styles.css">
+        <script src="https://unpkg.com/htmx.org@1.9.10" integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous"></script>
     </head>
     
     ${(
