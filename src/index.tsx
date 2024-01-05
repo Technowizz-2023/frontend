595537ba0c66ac29003d3553b4b559aace8a1d02
Jaromír Trnka <JrTrnka55@gmail.com>
2024-01-05 18:30:04 +0100

    formated and added some playground behavior

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,19 @@ import { Elysia, t } from "elysia";
 import { html } from "@elysiajs/html";
 import * as elements from "typed-html";
 
+import { BaseHtml } from "./components/BaseHTML.tsx";
+
 const app = new Elysia()
     .use(html())
 
-    .get("/", ({ html }) => html(<BaseHtml />))
-    .get("/hi", () => "hi")
+    .get("/", () => <BaseHtml />)
+
+    .post("/", () => <BaseHtml />)
+    .post("/hi", () => (
+        <button class="text-slate-100" hx-post="/" hx-swap="outerHTML">
+            this
+        </button>
+    ))
 
     .get("styles.css", () => Bun.file("./src/assets/css/output.css"))
 
@@ -19,26 +27,3 @@ const app = new Elysia()
             console.log(`🦊 Elysia is running at http://${hostname}:${port}`);
         }
     );
-
-export const BaseHtml = () => `
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>BUNJS</title>
-        <link rel="stylesheet" href="styles.css">
-        <script src="https://unpkg.com/htmx.org@1.9.10" integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous"></script>
-    </head>
-    
-    ${(
-        <body class="grid justify-center gap-4 bg-slate-950 py-8">
-            <h1 class="mb-4 text-3xl font-semibold text-slate-100">Hello</h1>
-            <button hx-post="/hi" hx-swap="outerHTML" class="text-slate-100">
-                button
-            </button>
-        </body>
-    )}
-</html>
-`;
